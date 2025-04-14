4f531bea963e3253cc383e3ee30d63c78b771cd1
ST10082707 <102595403+Chloemakhale@users.noreply.github.com>
2025-04-14 22:46:54 +0200

    testing chatbot

--- Program.cs
+++ Program.cs
@@ -11,6 +11,21 @@ namespace ChatBot
         static void Main(string[] args)
         {
             // Hi my name is Chloe, and this is my first commit
+            Console.WriteLine("Hi, I am VC chatbot. What is your name?");
+            Console.WriteLine("");
+            string username = Console.ReadLine();
+            Console.WriteLine("");
+            Console.WriteLine("Hello " + username+"! I hope you are well. How can I help you?");
+
+            string text = Console.ReadLine();
+
+            if (text.Contains("cybersecurity"))
+            {
+                Console.WriteLine("Cybersecurity is the staple of the online world.");
+            }
+            else {
+                Console.WriteLine("Goodbye...");
+            }
         }
     }
 }
