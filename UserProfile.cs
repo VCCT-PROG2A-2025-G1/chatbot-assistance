89a372311c35a3a7f43bc7409101d0de6f895097
ST10082707 <102595403+Chloemakhale@users.noreply.github.com>
2025-05-26 19:31:00 +0200

    Stores user data

---
+++ UserProfile.cs
@@ -0,0 +1,12 @@
+﻿namespace ChatBot
+{
+    /// <summary>
+    /// Method will declare all user input/details throughout the chats
+    /// </summary>
+    internal static class UserProfile
+    {
+        public static string Name { get; set; }
+        public static string FavoriteTopic { get; set; }
+        public static string Interest { get; set; }
+    }
+}
